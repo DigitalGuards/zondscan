5dfb4635163c81068c3e419a31d9ad17da18d1ab
moscowchill <harald@langmar.nl>
2025-02-25 21:22:41 +0100

    chore: Update TestnetV1 node port and add nul to .gitignore
    - Modify deployment scripts to use updated TestnetV1 node port (32837)
    - Add 'nul' to .gitignore to prevent tracking of temporary Windows files
    - Update log file with synchronization details for the new node configuration

--- deploy-windowsgitbash.sh
+++ deploy-windowsgitbash.sh
@@ -55,7 +55,7 @@ check_mongodb() {
 select_node() {
     print_status "Select Zond node to use:"
     PS3="Please choose the node (1-3): "
-    options=("Local node (127.0.0.1:8545)" "Remote node (REDACTED:8545)" "TestnetV1 node (35.158.17.89:32813)")
+    options=("Local node (127.0.0.1:8545)" "Remote node (REDACTED:8545)" "TestnetV1 node (35.158.17.89:32837)")
     select opt in "${options[@]}"
     do
         case $opt in
